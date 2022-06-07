e4424ad6ef3c0cdd521a7c9d3cc15b15e7f861ce
Ben Botsford <ben.botsford@flatironschool.com>
2022-06-07 15:46:07 -0500

    rspectful tests

--- testing/app_test.py
+++ testing/app_test.py
@@ -5,24 +5,28 @@ import runpy
 import io
 import sys
 
-def test_app_py_exists():
+class TestAppPy:
     '''
-    app.py exists in current directory
+    app.py
     '''
-    assert(path.exists("app.py"))
+    def test_app_py_exists(self):
+        '''
+        exists in current directory
+        '''
+        assert(path.exists("app.py"))
 
-def test_app_py_runs():
-    '''
-    app.py is executable
-    '''
-    runpy.run_path("app.py")
+    def test_app_py_runs(self):
+        '''
+        is executable
+        '''
+        runpy.run_path("app.py")
 
-def test_prints_hello_world():
-    '''
-    app.py prints "Hello World! Pass this test, please."
-    '''
-    captured_out = io.StringIO()
-    sys.stdout = captured_out
-    runpy.run_path("app.py")
-    sys.stdout = sys.__stdout__
-    assert(captured_out.getvalue() == "Hello World! Pass this test, please.\n")
+    def test_prints_hello_world(self):
+        '''
+        prints "Hello World! Pass this test, please."
+        '''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        runpy.run_path("app.py")
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "Hello World! Pass this test, please.\n")
