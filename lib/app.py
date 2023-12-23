7b6b4775c3e6ca2be3d184c87c08b91355b67942
Elaine <elainedoud@gmail.com>
2023-12-23 17:56:11 -0500

    A couple practice lines

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+
+print("Hello World! Pass this test, please.")
+# This was the python lab assignment
+
+
+
+def greeting (name,day) :
+    print(f"Greetings {name}! How are you this fine {day}?")
+
+# We are expecting the input to be the user's name and the day of the week.
+# The greeting function is sample code for my blog post
